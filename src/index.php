c8c0cfa8a46ee2a6847d90d56204732051b6ae63
Marcus Ball <marcus.ball@live.com>
2021-05-27 01:16:29 -0400

    Add Slim and PSR 7 requirements

--- src/index.php
+++ src/index.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-echo 'Hello World';
+require __DIR__ . '/../vendor/autoload.php';
 
-?>
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/test', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello!");
+    return $response;
+});
+
+$app->run();
